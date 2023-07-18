9e0a78005a971e2f44408178cd5c8102309cd9f6
Erwin Heitzman <15839059+erwinheitzman@users.noreply.github.com>
2023-07-18 15:39:06 +0200

    decrease timeout

--- test.e2e.js
+++ test.e2e.js
@@ -1,7 +1,7 @@
 describe('My Login application', () => {
     it('should show two squares in the screenshot', async () => {
         await browser.url(`file:///${__dirname}/index.html`)
-        await browser.pause(3000)
+        await browser.pause(1000)
         await browser.checkFullPageScreen();
     })
 })
